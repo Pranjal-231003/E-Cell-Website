b1bf84804a2c49a2c3017f5a5716e157eb9508fc
Jay <124623992+jay9358@users.noreply.github.com>
2023-10-20 00:29:49 +0530

    landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import Landingpage from './components/Landingpage';
 
 function App() {
   return (
     <>
+    <Landingpage></Landingpage>
     </>
   );
 }
---
+++ frontend/src/components/Landingpage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./assets/css/Landingpage.css"
+import image from "./assets/image/image 5.png"
+function Landingpage() {
+  return (
+    <>
+    <div className="Ecell_container">
+        <h1 className='club_name'>ENTRPRENEURSHIP</h1>
+        <img src={image} alt="" srcset="" />
+        <h1 className='stroke'>ENTRPRENEURSHIP</h1>
+        <h1 className='cell'>CELL</h1>
+    </div>
+    </>
+  )
+}
+
+export default Landingpage
