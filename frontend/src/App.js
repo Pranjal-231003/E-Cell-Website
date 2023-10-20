83e946eed9d30e9547d5acfbace536adc624f9d5
Ayush Bansal <116891199+Bansalayush247@users.noreply.github.com>
2023-10-20 15:50:50 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
 import Landingpage from './components/Landingpage';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
     <>
+  <Navbar></Navbar>
     <Landingpage></Landingpage>
+    
     </>
   );
 }
