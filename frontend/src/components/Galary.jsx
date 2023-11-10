86d4dd0f9395dc1397f58ea69266d4ec4e630979
Jay <124623992+jay9358@users.noreply.github.com>
2023-11-11 01:31:26 +0530

    done

--- frontend/src/components/Galary.jsx
+++ frontend/src/components/Galary.jsx
@@ -8,9 +8,18 @@ import img5 from './assets/image/imge (5).jpg';
 import img6 from './assets/image/imge (6).jpg';
 
 function Gallery() {
+    const [isGalleryClicked, setIsGalleryClicked] = useState(false);
 
-
-
+    const handleonclick=()=>{
+        
+            setIsGalleryClicked(!isGalleryClicked);
+       
+    }
+    const g3Style = {
+        transform: isGalleryClicked
+          ? 'translate3d(0px, 20px, 0px) scale3d(1.2, 1.4, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)'
+          : 'translate3d(0px, 0px, 0px) scale3d(0.5, 0.5, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
+      };
   return (
     <div className="gallery_contain">
         <div className="grid_parent">
@@ -26,15 +35,23 @@ function Gallery() {
             <div className="grid"></div>
         </div>
         <div className="text_parent">
-            <div className="gallary_parent">
+            <div className="gallary_parent" onClick={handleonclick}>
                 <h2>Gallary</h2>
                 <div className="view">view Gallary</div>
             </div>
         </div>
         <div className="grid_2">
-            < div className='placement_1 g3'></div>
-            < div className='placement_2 g4'></div>
-            < div className='placement_3 g5'></div>
+            < div className={`placement_3 g3 ${isGalleryClicked ? 'animated' : ''}`}></div>
+            < div className={`placement_2 g4 ${isGalleryClicked ? 'animated' : ''}`}></div>
+            < div className={`placement_3 g5 ${isGalleryClicked ? 'animated' : ''}`}></div>
+            <div className={`placement_2 g6  ${isGalleryClicked ? 'animated' : ''}`}></div>
+            <div className={`placement_2 g7 ${isGalleryClicked ? 'animated' : ''}`}></div>
+            <div className={`placement_2 g11 ${isGalleryClicked ? 'animated' : ''}`}></div>
+            <div className={`placement_2 g8 ${isGalleryClicked ? 'animated' : ''}`}></div>
+            <div className={`placement_3 g9 ${isGalleryClicked ? 'animated' : ''}`}></div>
+            <div className={`placement_1 g10 ${isGalleryClicked ? 'animated' : ''}`}></div>
+            <div className={`placement_3 g12 ${isGalleryClicked ? 'animated' : ''}`}></div>
+
         </div>
    
     </div>
