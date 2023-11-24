3b9c7ddb599391d9d422b4607a833708aa65abe2
AdiRocks007 <adityakumar290304@gmail.com>
2023-11-24 23:31:05 +0530

    Merge changes from remote branch

--- src/App.js
+++ src/App.js
@@ -46,44 +46,44 @@ function App() {
         <div id="slide">
           <div className="item-s" style={{ backgroundImage: 'url(assets/images/3pg/Content\ Management.png)' }}>
             <div className="content">
-              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '475%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>SHARK TANK 1</div>
-              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>WE OVERSEE YOUR SOCIAL MEDIA CONTENT DISTRIBUTION, WITH OPTIMAL CAPTIONS AND ENGAGEMENT COMPONENTS THAT AMPLIFY YOUR VISIBILITY AND ENGAGEMENT.</div>
+              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '475%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>DISRUPT</div>
+              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>Disrupt is a platform where the institute's budding entrepreneurs can pitch their startups to a panel of industry experts and potential investors. This platform offers comprehensive feedback and support, with the aim of enhancing their development and securing funding.</div>
             </div>
           </div>
           <div className="item-s" style={{ backgroundImage: 'url(assets/images/3pg/Ideation\ and\ Consulting.png)' }}>
             <div className="content">
-              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '475%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>IDEATION &amp; CONSULTING 2</div>
-              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>WE OVERSEE YOUR SOCIAL MEDIA CONTENT DISTRIBUTION, WITH OPTIMAL CAPTIONS AND ENGAGEMENT COMPONENTS THAT AMPLIFY YOUR VISIBILITY AND ENGAGEMENT.</div>
+              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '475%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>YELLOW PAGES</div>
+              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>Yellow Pages is a contest where participants build products or services within a limited time frame and demonstrate their financial and marketing strategies. They have to prove to the judges that their product can generate revenue and attract customers.</div>
             </div>
           </div>
           <div className="item-s" style={{ backgroundImage: 'url(assets/images/3pg/Influencer\ Marketing.png)' }}>
             <div className="content">
-              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '475%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>INFLUENCER MARKETING 3</div>
-              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>WE OVERSEE YOUR SOCIAL MEDIA CONTENT DISTRIBUTION, WITH OPTIMAL CAPTIONS AND ENGAGEMENT COMPONENTS THAT AMPLIFY YOUR VISIBILITY AND ENGAGEMENT.</div>
+              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '475%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>SHARK TANK</div>
+              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>Shark Tank provided aspiring entrepreneurs with a distinctive opportunity to present their innovative ideas and skills to investors and venture capitalists. In response to their promising pitches, they secured seed funding exceeding 25 lakhs, marking a pivotal moment in their entrepreneurial journey.</div>
             </div>
           </div>
           <div className="item-s" style={{ backgroundImage: 'url(assets/images/3pg/Video\ Editor.png)' }}>
             <div className="content">
-              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '525%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>STUNNING VISUALS 4</div>
-              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>WE OVERSEE YOUR SOCIAL MEDIA CONTENT DISTRIBUTION, WITH OPTIMAL CAPTIONS AND ENGAGEMENT COMPONENTS THAT AMPLIFY YOUR VISIBILITY AND ENGAGEMENT.</div>
+              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '525%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>SELL WITH ECELL</div>
+              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>Sell with Ecell -An exceptional opportunity to showcase your selling expertise and captivate customers. Exercise autonomy by establishing your personalized stalls, enabling you to market your products and services according to your preferences and strategic planning, ensuring a dynamic and impactful presentation..</div>
             </div>
           </div>
           <div className="item-s" style={{ backgroundImage: 'url(assets/images/3pg/Video\ Editor.png)' }}>
             <div className="content">
-              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '525%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>STUNNING VISUALS 5</div>
-              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>WE OVERSEE YOUR SOCIAL MEDIA CONTENT DISTRIBUTION, WITH OPTIMAL CAPTIONS AND ENGAGEMENT COMPONENTS THAT AMPLIFY YOUR VISIBILITY AND ENGAGEMENT.</div>
+              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '525%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>IGNITE</div>
+              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>Ignite facilitates a dynamic platform wherein participants collaborate in real time to assemble groups and draw chits containing two words. Subsequently, participants construct a compelling pitch, which they then present before a discerning audience of angel investors(which are the members of E-CELL)</div>
             </div>
           </div>
           <div className="item-s" style={{ backgroundImage: 'url(assets/images/3pg/Video\ Editor.png)' }}>
             <div className="content">
-              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '525%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>STUNNING VISUALS 6</div>
-              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>WE OVERSEE YOUR SOCIAL MEDIA CONTENT DISTRIBUTION, WITH OPTIMAL CAPTIONS AND ENGAGEMENT COMPONENTS THAT AMPLIFY YOUR VISIBILITY AND ENGAGEMENT.</div>
+              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '525%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>DEMYSTIFY</div>
+              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>Demystify represents a case study competition that revolves around the meticulous examination of a chosen unicorn company by participating teams. In this endeavor, teams delve deep into various facets of the selected company, including but not limited to its overarching vision, the founder, their revenue-generating methodologies, and the intricate intricacies of their market strategies.</div>
             </div>
           </div>
           <div className="item-s" style={{ backgroundImage: 'url(assets/images/3pg/Video\ Editor.png)' }}>
             <div className="content">
-              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '525%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>STUNNING VISUALS 7</div>
-              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>WE OVERSEE YOUR SOCIAL MEDIA CONTENT DISTRIBUTION, WITH OPTIMAL CAPTIONS AND ENGAGEMENT COMPONENTS THAT AMPLIFY YOUR VISIBILITY AND ENGAGEMENT.</div>
+              <div className="name" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '525%', lineHeight: '120%', fontWeight: 'normal', marginBottom: '12%' }}>STARTUP WEEKEND</div>
+              <div className="des" style={{ fontFamily: '"Bebas Neue", sans-serif !important', fontSize: '135%', marginBottom: '-20%' }}>Startup Weekend, in collaboration with Google , unfolds over a 3-day duration.</div>
             </div>
           </div>
         </div>
