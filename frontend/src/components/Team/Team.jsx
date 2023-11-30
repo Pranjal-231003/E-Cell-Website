d9b06184f4405237e6fd137c5e8a6d0f62f22267
Pranjal-2310 <pranjalshp2310@gmail.com>
2023-11-30 14:59:26 +0530

    fine tuning like hacker effect and images of some of us

--- frontend/src/components/Team/Team.jsx
+++ frontend/src/components/Team/Team.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import './Team.css';
 import TeamCard from './TeamCard';
-import img1 from '../assets/Team/logo512.png'
 import Varun from '../assets/Team/WhatsApp Image 2023-11-28 at 10.17.34 PM (1).jpeg';
 import Yash from '../assets/Team/WhatsApp Image 2023-11-28 at 10.17.34 PM.jpeg';
 import Bansal from '../assets/Team/WhatsApp Image 2023-11-28 at 10.17.33 PM.jpeg';
+import Shourya from '../assets/Team/IMG_5265.jpg';
+import Harsh from '../assets/Team/IMG-20230822-WA0103.jpg';
+import Jay from '../assets/Team/1701188174968.jpg';
+import Pranjal from '../assets/Team/picofme.jpg'
+import Mitali from '../assets/Team/IMG-20230909-WA0082~2.jpg';
 
 const teamMembers = [
+  { id: 1, name: 'Varun Ashok', title: 'Coordinator', photo: Varun },
   { id: 1, name: 'Yash Gautam', title: 'Coordinator', photo: Yash },
   { id: 1, name: 'Harsh Bansal', title: 'Coordinator', photo: Bansal },
-  { id: 1, name: 'Varun Ashok', title: 'Coordinator', photo: Varun },
-  { id: 2, name: 'Pranjal Jain', title: 'Web Developer', photo: Varun },
-  { id: 3, name: 'Kumar Harsh', title: 'Web Developer', photo: Varun },
-  { id: 4, name: 'Aditya Kumar', title: 'Web Developer', photo: Varun },
-  { id: 5, name: 'Shourya Gupta', title: 'UI/UX Designer', photo: Varun },
-  { id: 6, name: 'Jay Aggarwal', title: 'Web Developer', photo: Varun },
+  { id: 2, name: 'Pranjal Jain', title: 'Web Developer', photo: Pranjal },
+  { id: 3, name: 'Kumar Harsh', title: 'Web Developer', photo: Harsh },
+  { id: 4, name: 'Shourya Gupta', title: 'UI/UX Designer', photo: Shourya },
+  { id: 5, name: 'Hugesh Kumar', title: 'Web Developer', photo: Varun },
+  { id: 6, name: 'Jay Aggarwal', title: 'Web Developer', photo: Jay },
   { id: 7, name: 'Ayush Bansal', title: 'Web Developer', photo: Varun },
-  { id: 8, name: 'Mitali Manya', title: 'Web Developer', photo: Varun },
-  { id: 9, name: 'K', title: 'Web Developer', photo: Varun },
+  { id: 8, name: 'Mitali Manya', title: 'Web Developer', photo: Mitali},
+ 
 
 
 ];
 
 const Team = () => {
 
-  const group1 = teamMembers.filter(member => member.id >= 2 && member.id <= 5);
-  const group2 = teamMembers.filter(member => member.id >= 6 && member.id <= 9);
+  const group1 = teamMembers.filter(member => member.id >= 3 && member.id <= 5);
+  const group2 = teamMembers.filter(member => member.id >= 6 && member.id <= 8);
 
   return (
     <div className="team-container">
@@ -37,6 +41,11 @@ const Team = () => {
       </div>
       <div className="our-team">
       <h1>OUR <span className="text-team">TEAM.</span></h1>
+      <div className="team-details1">
+      {teamMembers.slice(3, 4).map((member) => (
+          <TeamCard key={member.id} {...member} />
+        ))}
+      </div>
       <div className="team-details">
           {group1.map((member) => (
             <TeamCard key={member.id} {...member} />
